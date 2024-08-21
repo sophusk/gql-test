313bc238d0bbc437ec46db6f7933412b5da99c1b
sophusk <sophuskruhoffer@gmail.com>
2024-08-21 15:25:44 +0200

    feat: Add placeholder graphql operations and Graphiql readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gql-test
+
+```
+ query {
+   events {
+    title
+    price
+  }
+ }
+```
+
+```
+mutation {
+  createEvent(eventInput: {title: "Derp", description: "Lorem" price: 10.50, date: "2024-08-21T12:41:51.490Z"}) {
+    title,
+    description
+  }
+}
+```
--- app.js
+++ app.js
@@ -5,13 +5,30 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 const schema = buildSchema(`
+type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
   type RootQuery {
-    events: [String!]!
+    events: [Event!]!
   }
 
   type RootMutation {
-    createEvent(name: String): String
+    createEvent(eventInput: EventInput): Event
   }
 
   schema {
@@ -28,11 +45,20 @@ app.use(
     schema,
     rootValue: {
       events: () => {
-        return ['Event 1', 'Event 2', 'Event 3'];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+
+        events.push(event);
+
+        return event;
       }
     },
     graphiql: true
