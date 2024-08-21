627cdc29b79e478e418ea1dce26b0251e006aa61
sophusk <sophuskruhoffer@gmail.com>
2024-08-21 13:34:27 +0200

    feat: Add simple graphql endpoint

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
+const schema = buildSchema(`
+  type RootQuery {
+    events: [String!]!
+  }
+
+  type RootMutation {
+    createEvent(name: String): String
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`);
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello world');
-});
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: {
+      events: () => {
+        return ['Event 1', 'Event 2', 'Event 3'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000);
