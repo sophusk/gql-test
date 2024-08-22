7378f91d8b2225a957fb0952f0cb44e695cdba96
sophusk <sophuskruhoffer@gmail.com>
2024-08-22 11:09:44 +0200

    feat: Add createEvent mongoose resolver

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require("mongoose");
 
+const Event = require("./models/event");
+
 const app = express();
 
 const events = [];
@@ -49,17 +51,21 @@ app.use(
         return events;
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
-          description: args.description,
+          description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
+          date: new Date(args.eventInput.date)
+        });
 
-        events.push(event);
-
-        return event;
+        return event.save()
+          .then(result => {
+            return { ...result._doc };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
@@ -67,7 +73,7 @@ app.use(
 );
 
 mongoose
-  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gczs3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gczs3.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
     app.listen(3000);
   })
